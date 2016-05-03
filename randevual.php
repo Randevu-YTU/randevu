04f2aae4d600c8396519ac2953fc1dc4114265ef
kontarcher <kasim.sagir@hotmail.com>
2016-05-03 22:21:13 +0300

    RandevuAlma (Adım1-2-3)

---
+++ randevual.php
@@ -0,0 +1,100 @@
+<html>
+<head>
+ <title>Randevu Al</title>
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+   <link rel="stylesheet" href="main.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body style="background-color: lightcyan">
+
+<div class="container">
+  <h2>Randevu Al</h2>
+  <ul class="nav nav-pills">
+    <li class="active"><a data-toggle="pill" href="#adim1">Adım 1</a></li>
+    <li><a data-toggle="pill" href="#adim2">Adım 2</a></li>
+    <li><a data-toggle="pill" href="#adim3">Adım 3</a></li>
+  </ul>
+  
+  <div class="tab-content">
+    <div id="adim1" class="tab-pane fade in active">
+      <h3>Adım 1</h3>
+      
+      
+  <form class="form-horizontal" role="form">
+    <div class="form-group">
+  <div class="dropdown">
+  
+  
+    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Tutorials
+    <span class="caret"></span></button>
+    <label class="control-label col-sm-2" for="email">Fakülte:</label>
+    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Elektrik-Elektronik</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Eğitim </a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">İnşaat</a></li>
+    </ul>
+  </div>
+  
+  
+    </div>
+    
+    <div class="form-group">
+<div class="dropdown">
+  
+  
+    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Tutorials
+    <span class="caret"></span></button>
+    <label class="control-label col-sm-2" for="email">Bölüm:</label>
+    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Bilgisayar</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">elektrik </a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">elektronik</a></li>
+    </ul>
+  </div>
+    </div>
+    <div class="form-group">        
+        <div class="dropdown">
+    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Tutorials
+    <span class="caret"></span></button>
+    <label class="control-label col-sm-2" for="email">hoca adı:</label>
+    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">ati</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">amaç </a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">elif</a></li>
+    </ul>
+  </div>
+    </div>
+  </form>
+  
+  
+    	</div>
+    <div id="adim2" class="tab-pane fade">
+      <h3>Adım 2</h3>
+      
+        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Tutorials
+    <span class="caret"></span></button>
+    <label class="control-label col-sm-2" for="email">Konu:</label>
+    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Ara-Proje</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Bitirme Projesi </a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Ders</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Diğer</a></li>
+    </ul>
+    </br>
+      <label class="control-label col-sm-2" for="email">Açıklama:</label>
+  </div>
+    
+    
+    <div id="adim3" class="tab-pane fade">
+      <h3>Adım 3</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
+
