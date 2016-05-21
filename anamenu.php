813c1c8501ba58ddc64ff811795f424210916a68
Oğuzhan Öztürk <oguzhanozturk1995@gmail.com>
2016-05-21 15:08:23 +0300

    sunumlar sonrası

--- anamenu.php
+++ anamenu.php
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-<html lang = "en">
-
-<head>
-
-</head>
-<style>
-    h2 {
-        text-align: center;
-        font-size: 40px;
-        letter-spacing: 2px;
-        font-family: "Arial Black", Gadget, sans-serif;
-        color:black;
-    }
-    ul {
-        list-style-type: none;
-        margin: auto;
-        padding: 0;
-        width: 1000px;
-        background-color: #d0d7b0;
-        line-height: 100px;
-        padding: 20px 20px 20px 20px;
-    }
-    li a {
-        display: block;
-        color: #000;
-        text-decoration: none;
-        border: 10px solid #d0d7b0;
-        text-align: center;
-        letter-spacing: 3px;
-        font-size: 40px;
-        font-family:"Comic Sans MS", cursive, sans-serif;
-    }
-</style>
-<body style="background-color:#0e7267">
-<?php
-session_start();
-if(!isset($_SESSION['id'])) {
-    header('Location: hata.php');
-}
-?>
-<h2>CE RANDEVU SİSTEMİ</h2>
-<div >
-    <ul>
-        <li><a href="randevual.php" style="background-color:green">Randevu Al</a></li>
-        <li><a href="randevu_gecmisi.php" style="background-color:blue">Randevu Geçmişi</a></li>
-        <li><a href="hesap_bilgileri.php" style="background-color:yellow">Hesap Bilgileri</a></li>
-        <li><a href="index.php" style="background-color:red">Çıkış</a></li>
-    </ul>
-</div>
-
-</body>
-=======
 <html>
 <head>
     <style>
@@ -153,5 +100,4 @@ for($i=-1;$i<19;$i++){
 echo"</table>";
 ?>
 </body>
->>>>>>> origin/master
 </html>
--- hata.php
+++ hata.php
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-<?php
-
-echo 'Bu sayfaya erişemiyorsunuz. Lütfen <a href="index.php">giriş yapin.</a>';
-
-=======
 <?php
 
 echo 'Bu sayfaya erişemiyorsunuz. Lütfen <a href="giris.php">giriş yapin.</a>';
 
->>>>>>> origin/master
 ?>
